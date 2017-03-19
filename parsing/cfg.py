4210fdde81708262d5af3d24e0f56f01553c540d
evamy <antriksh.don@gmail.com>
2017-03-18 23:51:29 -0500

    CKY Parser

---
+++ parsing/cfg.py
@@ -0,0 +1,48 @@
+import nltk
+import re
+
+
+class CFG(object):
+
+    def __init__(self, CFGFILE):
+        self.import_cfg(CFGFILE)
+
+    def import_cfg(self, FILE):
+        if FILE.split('.')[1] != 'cfg':
+            raise ValueError(
+                'Not a context free grammar file. Please enter a ".cfg" file')
+            return
+
+        file = open(FILE, 'r')
+
+        lines = file.read().split('\n')
+
+        self.rules = {}
+        for line in lines:
+            if re.split(' *->[ ]*', line)[0] in self.rules.keys():
+                for item in re.split(' *\|[ ]*', re.split(' *->[ ]*', line)[1]):
+                    self.rules[re.split(
+                        ' *->[ ]*', line)[0]].append(item)
+            else:
+                self.rules[re.split(' *->[ ]*', line)[0]] = re.split(' *\|[ ]*',
+                                                                     re.split(' *->[ ]*', line)[1])
+
+    def return_rule(self, tags):
+
+        tags = list(tags)
+        assert(type(tags) == list)
+
+        tag = ' '.join(tags)
+        applicable = []
+        for rule in self.rules.items():
+            if tag in rule[1]:
+                applicable.append(rule[0])
+
+        return applicable
+
+if __name__ == '__main__':
+
+    cfg = CFG('main.cfg')
+    print cfg.rules
+    print cfg.return_rule(['Noun'])
+    print cfg.return_rule(['I'])
---
+++ parsing/cky.py
@@ -0,0 +1,47 @@
+# CKY Parser
+from cfg import CFG
+
+import nltk
+import itertools
+import sys
+
+
+class CKY():
+
+    def display(self):
+        print 'Table'
+        for line in self.cky_table:
+            for cell in line:
+                sys.stdout.write(str(cell) + ' ')
+            print
+
+    def cky_parse(self, words, grammar):
+
+        self.cky_table = []
+        for j in range(0, len(words) + 1):
+            table = []
+            for i in range(0, len(words) + 1):
+                table.append([])
+            self.cky_table.append(table)
+
+        # self.display()
+        for j in range(1, len(words) + 1):
+            self.cky_table[j - 1][j] = list(set([x
+                                                 for x in grammar.return_rule([words[j - 1]])]))
+            for i in range(j - 1, -1, -1):
+                for k in range(i, j):
+                    a = self.cky_table[i][k]
+                    b = self.cky_table[k][j]
+
+                    c = [grammar.return_rule(r)[0]
+                         for r in itertools.product(a, b) if grammar.return_rule(r) != []]
+
+                    self.cky_table[i][j] = self.cky_table[i][j] + c
+
+        self.display()
+
+if __name__ == '__main__':
+    cfg = CFG('main.cfg')
+    cky = CKY()
+
+    cky.cky_parse(['I', 'am'], cfg)
